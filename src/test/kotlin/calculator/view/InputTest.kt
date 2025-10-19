fc405e7c94ddec0bd3b8559f5ddf5d939153fa2a
server <me@rungeun.space>
2025-10-19 18:24:40 +0900

    feat(input): 입력 기능 구현
    - 입력 테스트 코드 작성
    - 입력 View 구현
    - 총괄 Controller 구현

---
+++ src/test/kotlin/calculator/view/InputTest.kt
@@ -0,0 +1,22 @@
+package calculator
+
+import camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest
+import camp.nextstep.edu.missionutils.test.NsTest
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Test
+import calculator.view.ConsoleInputView
+
+class InputTest : NsTest() {
+    @Test
+    fun `사용자 입력`() {
+        assertSimpleTest {
+            run("1,2,3")
+        }
+    }
+
+    override fun runMain() {
+        val inputView = ConsoleInputView()
+        val result = inputView.readString()
+        assertThat(result).isEqualTo("1,2,3")
+    }
+}
